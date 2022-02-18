e21baf3f6db39b2ed58924332709b4034cf16383
Andrin Meier <andrin.meier@hispeed.ch>
2022-02-18 23:32:23 +0100

    add favicon

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,5 @@
 import { Container } from '@mui/material';
+import Head from 'next/head';
 import React from 'react';
 import Footer from './Footer';
 import NavBar from './NavBar';
@@ -7,6 +8,15 @@ const Layout = (props: any) => {
     const { children } = props;
     return (
         <div className='layout'>
+            <Head>
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest" />
+                <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#51279b" />
+                <meta name="msapplication-TileColor" content="#603cba" />
+                <meta name="theme-color" content="#ffffff" />
+            </Head>
             <NavBar />
             <main>
                 <Container>
