ec61a75df70e48f1a9bca7dc7e6eb1d40ef6c19a
Andrin Meier <andrin.meier@hispeed.ch>
2022-02-19 15:54:27 +0100

    add fullscreen support

--- pages/games/snake2d/index.tsx
+++ pages/games/snake2d/index.tsx
@@ -1,17 +1,21 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import { useEffect, useRef, useState, useCallback } from 'react';
+import { useRef, useState, useCallback } from 'react';
 import { Game } from 'meigames-snake2d';
 import Typography from '@mui/material/Typography';
 import Image from 'next/image'
 import snakeTitleImage from '../../../public/snake2d_title.png';
 import { Grid, IconButton, Paper } from '@mui/material';
 import ReplayIcon from '@mui/icons-material/Replay';
+import { FullScreen, useFullScreenHandle } from "react-full-screen";
+import FullscreenIcon from '@mui/icons-material/Fullscreen';
+import FullscreenExitIcon from '@mui/icons-material/FullscreenExit';
 
 const Snake2DPage: NextPage = () => {
   const canvasRef = useRef(null);
   const [score, setScore] = useState(0);
   const [status, setStatus] = useState("Idle");
+  const fullScreenHandle = useFullScreenHandle();
 
   const startGame = useCallback(() => {
     if (!canvasRef.current) {
@@ -53,7 +57,21 @@ const Snake2DPage: NextPage = () => {
               alt="Snake 2D" />
           </Grid>
           <Grid item xs={12}>
-            <canvas ref={canvasRef} id="snake2d-canvas" style={{ border: "4px solid #51279B", borderRadius: "0.5em", width: "100%" }} />
+            <FullScreen handle={fullScreenHandle}>
+              <Grid container>
+                <Grid item xs={12}>
+                  <canvas ref={canvasRef} id="snake2d-canvas" style={{ border: "4px solid #51279B", borderRadius: "0.5em", width: "100%" }} />
+                </Grid>
+                <Grid item xs={12} visibility={ fullScreenHandle.active ? "visible" : "hidden" }>
+                  <IconButton color="primary" aria-label="Restart game" component="span" onClick={startGame}>
+                    <ReplayIcon />
+                  </IconButton>
+                  <IconButton color="primary" aria-label="Restart game" component="span" onClick={fullScreenHandle.exit}>
+                    <FullscreenExitIcon />
+                  </IconButton>
+                </Grid>
+              </Grid>
+            </FullScreen>
           </Grid>
           <Grid item xs={12}>
             <Typography component="div" variant="h5">
@@ -76,8 +94,11 @@ const Snake2DPage: NextPage = () => {
               <br />
               Try to eat as much food as you can without hitting yourself or the wall.
             </Typography>
-            <IconButton color="primary" aria-label="upload picture" component="span" onClick={startGame}>
+            <IconButton color="primary" aria-label="Restart game" component="span" onClick={startGame}>
               <ReplayIcon />
+            </IconButton>
+            <IconButton color="primary" aria-label="Enter Full-Screen Mode" component="span" onClick={fullScreenHandle.enter}>
+              <FullscreenIcon />
             </IconButton>
           </Grid>
         </Grid>
