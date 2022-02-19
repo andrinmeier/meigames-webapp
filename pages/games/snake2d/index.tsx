424850a54eb9c035937b38b83e37d0ae4bb09b59
Andrin Meier <andrin.meier@hispeed.ch>
2022-02-19 01:14:48 +0100

    update snake2d design

--- pages/games/snake2d/index.tsx
+++ pages/games/snake2d/index.tsx
@@ -4,10 +4,17 @@ import { useEffect, useRef, useState } from 'react';
 import { Game } from 'meigames-snake2d';
 import Image from 'next/image';
 import titleImage from '../../../public/snake2d_title.png';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import RestartAltIcon from '@mui/icons-material/RestartAlt';
 
 const Snake2DPage: NextPage = () => {
   const canvasRef = useRef(null);
   const [score, setScore] = useState(0);
+  const [status, setStatus] = useState("Running");
 
   useEffect(() => {
     if (!canvasRef.current) {
@@ -21,7 +28,7 @@ const Snake2DPage: NextPage = () => {
       setScore(score => score + 1);
     });
     game.registerOnGameDone(() => {
-      setScore(score => -1);
+      setStatus("Game over");
     })
     game.start();
   }, [canvasRef]);
@@ -44,9 +51,36 @@ const Snake2DPage: NextPage = () => {
       </Head>
       <Image
         src={titleImage}
-        alt="Snake 2D"/>
-      <p>{score}</p>
-      <canvas ref={canvasRef} id="snake2d-canvas" style={{ width: "100%", height: "100%" }} />
+        alt="Snake 2D" />
+      <Card sx={{ display: 'flex', marginTop: 5 }}>
+        <Box sx={{ padding: 3, width: "512px", height: "512px" }}>
+          <canvas ref={canvasRef} id="snake2d-canvas" style={{ border: "4px solid #51279B", borderRadius: "0.5em", width: "100%", height: "100%" }} />
+        </Box>
+        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+          <CardContent sx={{ flex: '1 0 auto' }}>
+            <Typography component="div" variant="h5">
+              Score
+            </Typography>
+            <Typography variant="subtitle1" color="text.secondary" component="div">
+              {score}
+            </Typography>
+            <Typography component="div" variant="h5">
+              Status
+            </Typography>
+            <Typography variant="subtitle1" color="text.secondary" component="div">
+              {status}
+            </Typography>
+            <Typography component="div" variant="h5">
+              How to play (desktop)
+            </Typography>
+            <Typography variant="subtitle1" color="text.secondary" component="div">
+              Use <b>A</b> to rotate left and <b>D</b> to rotate left.
+              <br/>
+              Try to eat as much food as you can without hitting yourself or the wall.
+            </Typography>
+          </CardContent>
+        </Box>
+      </Card>
     </div>
   )
 }
