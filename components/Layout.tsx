4a8ad822672d68bba4aa2e1c46c2133aca8e7951
Andrin Meier <andrin.meier@hispeed.ch>
2022-02-21 00:06:25 +0100

    add plausible

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,26 +1,29 @@
-import { Box, Container } from '@mui/material';
+import { Box } from '@mui/material';
 import Head from 'next/head';
 import React from 'react';
+import PlausibleProvider from 'next-plausible';
 
 const Layout = (props: any) => {
     const { children } = props;
     return (
-        <div className='layout'>
-            <Head>
-                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-                <link rel="manifest" href="/site.webmanifest" />
-                <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#51279b" />
-                <meta name="msapplication-TileColor" content="#603cba" />
-                <meta name="theme-color" content="#ffffff" />
-            </Head>
-            <main>
-                <Box sx={{ margin: 5 }}>
-                    {children}
-                </Box>
-            </main>
-        </div>
+        <PlausibleProvider domain="mei-games.com">
+            <div className='layout'>
+                <Head>
+                    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                    <link rel="manifest" href="/site.webmanifest" />
+                    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#51279b" />
+                    <meta name="msapplication-TileColor" content="#603cba" />
+                    <meta name="theme-color" content="#ffffff" />
+                </Head>
+                <main>
+                    <Box sx={{ margin: 5 }}>
+                        {children}
+                    </Box>
+                </main>
+            </div>
+        </PlausibleProvider >
     )
 }
 
