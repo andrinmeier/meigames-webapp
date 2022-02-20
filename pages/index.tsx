34999342d0a131b19db3285bdaaaacb7210dec83
Andrin Meier <andrin.meier@hispeed.ch>
2022-02-20 23:03:00 +0100

    switch to simple grids for responsiveness

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head';
 import Image from 'next/image'
 import snakeTitleImage from '../public/snake2d_title.png';
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItem, { imageListItemClasses } from '@mui/material/ImageListItem';
 import { Link, Grid, Box } from '@mui/material';
 import MeiLogo from '../components/MeiLogo';
 import Typography from "@mui/material/Typography";
@@ -27,29 +26,21 @@ const Home: NextPage = () => {
           key="desc"
         />
       </Head>
-      <ImageList
-        variant="quilted"
-        cols={4}
-      >
-        <ImageListItem key={2} cols={1} rows={2} sx={{ border: "5px solid #0E5814", borderRadius: "5px", padding: 3, background: "#E1E1E1" }}>
-          <Grid container>
-            <Grid item xs={12}>
-              <MeiLogo sx={{ fill: "#207227", width: "100%", height: "100%" }} />
-            </Grid>
-            <Grid item xs={12}>
-              <Typography color="secondary.dark" variant="h2" sx={{ fontWeight: "bold" }}>MeiGames</Typography>
-            </Grid>
-          </Grid>
-        </ImageListItem>
-        <ImageListItem key={1} cols={1} rows={1} sx={{ border: "5px solid #0E5814", borderRadius: "5px", padding: 3, background: "#E1E1E1" }}>
+      <Grid container spacing={0} alignItems="center" justifyContent="center">
+        <Grid item xs={12} sm={3} sx={{ border: "2px solid #0E5814", borderRadius: "5px", padding: 3, margin: 3, background: "#E1E1E1" }}>
+          <MeiLogo sx={{ fill: "#207227", width: "100%", height: "100%" }} />
+          <br/>
+          <Typography color="secondary.dark" variant="h4" sx={{ textAlign: "center", fontWeight: "bold" }}>MeiGames</Typography>
+        </Grid>
+        <Grid item xs={12} sm={5} sx={{ border: "2px solid #0E5814", borderRadius: "5px", padding: 3, margin: 3, background: "#E1E1E1" }}>
           <Link href="/games/snake2d">
             <Image
               src={snakeTitleImage}
               alt="Snake 2D"
               placeholder="blur" />
           </Link>
-        </ImageListItem>
-        <ImageListItem key={2} cols={1} rows={1} sx={{ border: "5px solid #0E5814", borderRadius: "5px", padding: 3, background: "#E1E1E1" }}>
+        </Grid>
+        <Grid item xs={12} sm={4} sx={{ border: "2px solid #0E5814", borderRadius: "5px", padding: 3, margin: 3, background: "#E1E1E1" }}>
           <Box>
             <Typography variant="body1">Developed by <Link color="inherit" underline="none" href="mailto:play@mei-games.com">Andrin Meier</Link></Typography>
             <Typography variant="body1">Drawings by <Link color="inherit" underline="none" href="mailto:artist@mei-games.com">麻婷娜</Link></Typography>
@@ -57,9 +48,9 @@ const Home: NextPage = () => {
               <GitHubIcon fontSize="large" />
             </IconButton>
           </Box>
-        </ImageListItem>
-      </ImageList>
-    </div>
+        </Grid>
+      </Grid>
+    </div >
   )
 }
 
